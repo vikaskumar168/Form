a2e324d2b4599d98a12a0367b53935fd4c10fd3e
vikas <vikaskmr2411@gmail.com>
2023-09-26 13:54:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+
+  const formChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    setFormErrors({ ...formErrors, [name]: '' }); 
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const newFormErrors = validateForm(formData);
+    setFormErrors(newFormErrors);
+    if (Object.values(newFormErrors).every((error) => error === '')) {
+      console.log('Form submitted:', formData);
+    }
+  };
+
+  const validateForm = (values) => {
+    const errors = {};
+    if (values.firstName.trim() === '') {
+      errors.firstName = 'First Name can not be empty';
+    }
+    if (values.lastName.trim() === '') {
+      errors.lastName = 'Last Name can not be empty';
+    }
+    if (values.email.trim() === '') {
+      errors.email = 'Email section is empty';
+    } 
+    else if (!isValidEmail(values.email)) {
+      errors.email = 'Invalid email format';
+    }
+    if (values.password.trim() === '') {
+      errors.password = 'Password must be at least 6 characters long';
+    } 
+    else if (values.password.length < 6) {
+      errors.password = 'Password must be at least 6 characters long';
+    } 
+    else if (/^\d+$/.test(values.password) || /^[a-zA-Z]+$/.test(values.password)) {
+      errors.password = 'Password must contain a mix of letters and numbers';
+    }
+
+    return errors;
+  };
+  const isValidEmail = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Login Form</h1>
+      <form onSubmit={formSubmit}>
+        <div>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            onChange={formChange}
+          />
+          <div className="error">{formErrors.firstName}</div>
+        </div>
+        <div>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            onChange={formChange}
+          />
+          <div className="error">{formErrors.lastName}</div>
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={formChange}
+          />
+          <div className="error">{formErrors.email}</div>
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={formChange}
+          />
+          <div className="error">{formErrors.password}</div>
+        </div>
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
